38a85a69ba62a894abfa2179b562cf69ae13269f
el10savio <el10savio96@gmail.com>
2021-01-27 17:13:43 +0530

    Added RGA AddBetween Functionality

--- rga/rga.go
+++ rga/rga.go
@@ -3,7 +3,7 @@ package rga
 // Element ...
 type Element struct {
 	Value      string
-	Identifier string
+	Identifier float64
 }
 
 // Sequence ...
@@ -27,15 +27,35 @@ func (sequence *Sequence) Lookup(element Element) bool {
 }
 
 // Decompose ...
-func (sequence *Sequence) Decompose(element Element) (string, string) {
+func (sequence *Sequence) Decompose(element Element) (string, float64) {
 	// TODO: Err Handle Element Not Present
 	return element.Value, element.Identifier
 }
 
 // Before ...
-func (sequence *Sequence) Before(element, elementNext Element) bool {
+func (sequence *Sequence) Before(elementPrevious, elementNext Element) bool {
 	// TODO: Err Handle Elements Not Present
-	_, identifier := sequence.Decompose(element)
+	_, identifierPrevious := sequence.Decompose(elementPrevious)
 	_, identifierNext := sequence.Decompose(elementNext)
-	return identifier < identifierNext
+	return identifierPrevious < identifierNext
+}
+
+// AllocateIdentifierBetween ...
+func AllocateIdentifierBetween(identifier1, identifier2 float64) float64 {
+	// TODO: Err Handle identifier1 > identifier2
+	return (identifier1 + identifier2) / 2
+}
+
+// AddBetween ...
+func (sequence *Sequence) AddBetween(value string, elementPrevious, elementNext Element) Element {
+	// TODO: Err handle Before(elementPrevious, elementNext) is false
+	_, identifierPrevious := sequence.Decompose(elementPrevious)
+	_, identifierNext := sequence.Decompose(elementNext)
+
+	element := Element{Value: value, Identifier: AllocateIdentifierBetween(identifierPrevious, identifierNext)}
+
+	// TODO: Add element to Sequence.Set
+	// TODO: Downstream
+
+	return element
 }
